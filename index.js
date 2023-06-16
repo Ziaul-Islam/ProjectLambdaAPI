76b1a3e0b82d917dbe4bdbc7c2547338d0805cff
Ziaul Islam <zia.isl@outlook.com>
2023-06-15 22:24:01 -0500

    major update

---
+++ index.js
@@ -0,0 +1,32 @@
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', async () => {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const requestBody = {
+    name: name,
+    email: email,
+    message: message
+  };
+
+  try {
+    const response = await fetch('https://3kxnugbjfg.execute-api.ca-central-1.amazonaws.com/Prod', {
+      method: 'POST',
+      body: JSON.stringify(requestBody)
+    });
+
+    if (response.ok) {
+      // Handle success response
+      alert('Data saved successfully.');
+      // Reset form fields if needed
+    } else {
+      // Handle error response
+      alert('Error saving data to API.');
+    }
+  } catch (error) {
+    // Handle network or other errors
+    alert('Error:', error.message);
+  }
+});
